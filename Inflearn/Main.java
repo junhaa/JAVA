e0ef367667bf9dfa54bda252ae187dbd3ea3d5f5
junhaa <94986147+junhaa@users.noreply.github.com>
2022-03-31 01:33:35 -0700

    Add files via upload

---
+++ Inflearn/Main.java
@@ -0,0 +1,26 @@
+package codingTest_072;
+
+import java.util.Scanner;
+
+public class Main {
+	static int answer = 0 , N;
+	public void DFS(int L) {
+		if(L == N) {
+			answer++;
+		}
+		else if(L > N) return;
+		else {
+			for(int i = 1 ; i <= 2 ; i ++) {
+				DFS(L+i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		T.DFS(0);
+		System.out.println(answer);                                            
+	}
+}
