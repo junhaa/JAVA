c78bc0a1db05cbd91bd679cce6a0e2c9087f13bc
junhaa <94986147+junhaa@users.noreply.github.com>
2022-07-14 00:51:18 +0900

    Add files via upload

---
+++ BOJ/BOJ2338.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+// BOJ #2338 긴자리 계산
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		BigInteger A = new BigInteger(br.readLine());
+		BigInteger B = new BigInteger(br.readLine());
+		
+		System.out.println(A.add(B));
+		System.out.println(A.subtract(B));
+		System.out.println(A.multiply(B));
+	}
+}
