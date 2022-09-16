a71a79fdce01c1bf07aef2ece31c456894da4528
junhaa <94986147+junhaa@users.noreply.github.com>
2022-09-16 18:26:35 +0900

    Add files via upload

---
+++ BOJ/BOJ11399.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// BOJ #11399 ATM
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N ; i ++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int sum = 0;
+		int last = 0;
+		Arrays.sort(arr);
+		for(int i = 0 ; i < N ; i ++) {
+			last += arr[i];
+			sum += last;
+		}
+		System.out.println(sum);
+	}
+}
