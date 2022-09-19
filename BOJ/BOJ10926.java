7b00f0b5c5eb51fc1e75d470074477adaa2e34b9
junhaa <94986147+junhaa@users.noreply.github.com>
2022-09-19 23:38:39 +0900

    Add files via upload

---
+++ BOJ/BOJ10926.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// BOJ #10926 ??!
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.println(input + "??!");
+	}
+}
