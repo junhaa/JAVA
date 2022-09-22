5e49359643f294fc70a5893cdf89b1fea8418e9f
junhaa <94986147+junhaa@users.noreply.github.com>
2022-09-22 20:25:13 +0900

    Add files via upload

---
+++ BOJ/BOJ2752.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// BOJ #2752 세수정렬
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[3];
+		for(int i = 0 ; i < 3; i ++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i = 0 ; i < 3 ; i ++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
