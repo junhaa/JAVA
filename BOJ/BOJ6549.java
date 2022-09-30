e05e30a2d36634396927779ba2ddc5558961794f
junhaa <94986147+junhaa@users.noreply.github.com>
2022-09-30 19:07:18 +0900

    Create BOJ6549.java

---
+++ BOJ/BOJ6549.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// BOJ #6549 히스토그램에서 가장 큰 직사각형
+public class Main {
+	static int[] arr;
+	
+	static long solution(int start, int end) {
+		if(start == end) return (long)arr[start];
+		
+		int mid = (start + end) / 2;
+		long answer = Math.max(solution(start, mid), solution(mid + 1, end));
+		
+		int lt = mid - 1, rt = mid + 1, h = arr[mid] ; 
+		long w, maxW = Integer.MIN_VALUE;
+		while(true){
+			if(lt != start - 1 && rt != end + 1) {
+				if(arr[lt] > arr[rt]) {
+					h = Math.min(h, arr[lt]);
+					w = ((long)rt - lt) * h;
+					maxW = Math.max(w, maxW);
+					lt --;
+				}
+				else {
+					h = Math.min(h, arr[rt]);
+					w = ((long)rt - lt) * h;
+					maxW = Math.max(w, maxW);
+					rt ++;
+				}
+			}
+			else if(lt == start - 1 && rt != end + 1) {
+				h = Math.min(h, arr[rt]);
+				w = ((long)rt - lt) * h;
+				maxW = Math.max(w, maxW);
+				rt ++;
+			}
+			else if(lt != start - 1 && rt == end + 1) {
+				h = Math.min(h, arr[lt]);
+				w = ((long)rt - lt) * h;
+				maxW = Math.max(w, maxW);
+				lt --;
+			}
+			else break;
+		}
+		return Math.max(maxW, answer);
+	}
+
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			if(n == 0) break;
+			arr = new int[n];
+			for(int i = 0 ; i < n ; i ++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			sb.append(T.solution(0, n - 1) + "\n");
+		}
+		System.out.println(sb);
+	}
+}
