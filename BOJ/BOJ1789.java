4c1ea7fc7cee10a1108d1ed40390fd43684b4bfb
hs-2171326-junhakim <94986147+junhaa@users.noreply.github.com>
2022-11-25 23:00:25 +0900

    Add files via upload

---
+++ BOJ/BOJ1789.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// BOJ #1789 수들의 합
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int S = Integer.parseInt(br.readLine());
+		int i = 1;
+		long sum = 0;
+		while(true) {
+			sum += i;
+			if(sum > S) {
+				System.out.println(i - 1);
+				return;
+			}
+			i ++;
+		}
+	}
+}
