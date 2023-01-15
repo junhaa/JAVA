1d1b67a7fe6053d2a0e731665375b339a48e4ede
junhaa <94986147+junhaa@users.noreply.github.com>
2023-01-15 21:14:41 +0900

    Update and rename BOJ2023A.java to BOJ27231.java

--- BOJ/BOJ27231.java
+++ BOJ/BOJ27231.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 
-// BOJ #A - 2023년이 기대되는 이유
+// BOJ #27231 2023년이 기대되는 이유
 public class Main {
 
 	static int[] arr;
