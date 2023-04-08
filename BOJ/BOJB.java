6f491680b68947b669f5ee6c6e3e1d890893bab2
junhaa <94986147+junhaa@users.noreply.github.com>
2023-04-08 14:50:11 +0900

    Add files via upload

---
+++ BOJ/BOJB.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// BOJ #B
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int cur = 0;
+		boolean flag = false;
+		int i;
+		for(i = 0 ; i < M ; i ++) {
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			cur += Integer.parseInt(st.nextToken());
+			if(cur > K) { 
+				flag = true;
+				break;
+			}
+		}
+		if(flag) System.out.println(++i + " 1");
+		else System.out.println(-1);
+	}
+}
