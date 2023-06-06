ebc119dac51a62f33a4e2f7a0c93253c46ed730a
junhaa <94986147+junhaa@users.noreply.github.com>
2023-06-06 14:57:30 +0900

    Create BOJ16212.java

---
+++ BOJ/BOJ16212.java
@@ -0,0 +1,123 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// BOJ #16212 정열적인 정렬 
+public class Main {
+
+	private static class FastIO {
+		final private int BUFFER_SIZE = 1 << 16;
+		final private int INT_SIZE = 10;
+		private DataInputStream din;
+		private DataOutputStream dout;
+
+		private byte[] inbuffer;
+		private int inputbufferpointer, bytesread;
+		private byte[] outbuffer;
+		private int outputbufferpointer;
+		private byte[] bytebuffer;
+		
+		FastIO() {
+			din = new DataInputStream(System.in);
+			dout = new DataOutputStream(System.out);
+			inputbufferpointer = bytesread = outputbufferpointer = 0;
+			bytebuffer = new byte[INT_SIZE];
+			inbuffer = new byte[BUFFER_SIZE];
+			outbuffer = new byte[BUFFER_SIZE];
+			
+			
+		}
+		
+		public int nextInt() throws IOException { 
+            int ret = 0; 
+            byte b = read(); 
+            while (b <= ' ') 
+                b = read(); 
+            boolean isneg = (b == '-'); 
+            if (isneg) 
+                b = read(); 
+            do { 
+                ret = ret * 10 + b - '0';
+            } while ((b = read()) >= '0' && b <= '9'); 
+  
+            if (isneg) 
+                return -ret; 
+            return ret; 
+        }
+		
+		private byte read() throws IOException { 
+            if (inputbufferpointer == bytesread) 
+                fillbuffer(); 
+            return inbuffer[inputbufferpointer++]; 
+        }
+		
+		private void fillbuffer() throws IOException { 
+            bytesread = din.read(inbuffer, inputbufferpointer = 0, BUFFER_SIZE); 
+            if (bytesread == -1) 
+                inbuffer[0] = -1; 
+        }
+		
+		public void write(int i) {
+			if (i == 0) {
+				writebuffer((byte) '0');
+				return;
+			}
+
+			if (i < 0) {
+				writebuffer((byte) '-');
+				i = -i;
+			}
+
+			int index = 0;
+			while (i > 0) {
+				bytebuffer[index++] = (byte) ((i % 10) + '0');
+				i /= 10;
+			}
+
+			while (index-- > 0)
+				writebuffer(bytebuffer[index]);
+		}
+		
+		public void writels(int i) {
+			write(i);
+			writebuffer((byte)' ');
+		}
+		
+		private void writebuffer(byte b) {
+			if (outputbufferpointer == outbuffer.length) {
+				flushbuffer();
+			}
+			outbuffer[outputbufferpointer++] = b;
+		}
+		
+		private void flushbuffer() {
+			if (outputbufferpointer != 0) {
+				try {
+					dout.write(outbuffer, 0, outputbufferpointer);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+			outputbufferpointer = 0;
+		}		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		FastIO myIO = new FastIO();
+		int N = myIO.nextInt();
+		int[] arr = new int[N];
+		for(int i = 0 ; i < N ; i ++) {
+			arr[i] = myIO.nextInt();
+		}
+		Arrays.sort(arr);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < N ; i ++) {
+			sb.append(arr[i] + " ");
+		}
+		System.out.println(sb);
+	}
+}
