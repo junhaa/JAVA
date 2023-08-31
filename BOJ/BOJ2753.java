99ea52b2a92390c67c244ebd586b5244747d6719
junhaa <94986147+junhaa@users.noreply.github.com>
2023-08-31 23:34:37 +0900

    Create BOJ2753.java

---
+++ BOJ/BOJ2753.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int year = scanner.nextInt();
+        
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        
+        scanner.close();
+    }
+}
