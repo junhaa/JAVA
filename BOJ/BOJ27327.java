b35721d97bc9129ca4ef9906d6405e3365d380bb
junhaa <94986147+junhaa@users.noreply.github.com>
2023-09-21 01:13:23 +0900

    Create BOJ27327.java

---
+++ BOJ/BOJ27327.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+// BOJ #27327 時間 (Hour)
+public class Main {
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) * 24);
+    }
+}
