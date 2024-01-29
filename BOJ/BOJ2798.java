b9da4573f43390507fa0d9341473ad67c3d03bdb
junhaa <94986147+junhaa@users.noreply.github.com>
2024-01-30 02:42:20 +0900

    Create BOJ2798.java

---
+++ BOJ/BOJ2798.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// BOJ #2798 블랙잭
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int min = Integer.MAX_VALUE;
+        int sum = -1;
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i ++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0 ; i < N ; i ++){
+            for(int j = i + 1 ; j < N ; j ++){
+                for(int k = j + 1 ; k < N ; k ++){
+                    int curSum = arr[i] + arr[j] + arr[k];
+                    int dif = M - curSum;
+                    if(dif < 0) continue;
+                    if(dif < min){
+                        min = dif;
+                        sum = curSum;
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
