8965fe8bc700be011e9f761d54721137d7cd1c78
junhaa <94986147+junhaa@users.noreply.github.com>
2024-05-15 02:47:04 +0900

    Create BOJ7891.java

---
+++ BOJ2/BOJ7891.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// BOJ #7891 Can you add this?
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a + b).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
